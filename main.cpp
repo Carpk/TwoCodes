147e6be1f14db5dc10a39b4b215c8ef70e9f46ac
Invalid <zappeln@gmail.com>
2020-03-18 13:30:15 -0500

    refactor code; abstracted out code to individual methods;

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <fstream>
 #include <cstring>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
@@ -17,34 +18,81 @@ void loadBook(string dictArray[ARRAYSIZE]) {
     while( getline(inStream,  line)) {
         if (line != "") {
             dictArray[i] = line;
-            i++;
+            i++;+
         }
     }
     inStream.close();
 }
 
-void prompt(){
+void prompt(bool isFullPrompt){
     cout << "\n"
-    << "Select from the following choices:\n"
-    << "W  Enter word or phrase to find, and display enclosing text\n"
-    << "P  Display previous text\n"
-    << "F  Display following text\n"
-    << "A  Display all occurrences of found word or phrase\n"
-    << "X  Exit program\n"
-    << "Your choice:" << endl;
+         << "Select from the following choices:\n"
+         << "W  Enter word or phrase to find, and display enclosing text" << endl;
+    if (isFullPrompt) {
+        cout << "P  Display previous text\n"
+             << "F  Display following text\n"
+             << "A  Display all occurrences of found word or phrase" << endl;
+    }
+    cout <<  "X  Exit program\n"
+         << "Your choice:" << endl;
 }
 
 
-vector<int> findText(string bookArray[ARRAYSIZE], string textToFind) {
+vector<int> findText(const string bookArray[ARRAYSIZE], string textToFind) {
     vector<int> foundIndices;
 
     for (int i = 0; i < ARRAYSIZE; i++) {
-        foundIndices = {1,2,3,4,5};
+        string parentStr = bookArray[i];
+        transform(parentStr.begin(), parentStr.end(), parentStr.begin(), ::tolower);
+
+        if (parentStr.find(textToFind) != string::npos) {
+            foundIndices.push_back(i);
+        }
     }
 
     return foundIndices;
 }
 
+vector<int> getIndices(const string bookArray[ARRAYSIZE],char userInput, bool isRunning) {
+    vector<int> foundIndices;
+    string userSearchTerm;
+
+    switch(toupper(userInput)) {
+        case 'W':
+            cout << "Enter word or phrase: " << endl;
+            userSearchTerm = "five years";
+            // convert userSearch to lowercase
+            foundIndices = findText(bookArray, userSearchTerm);
+            foundIndices = {foundIndices.at(0)};
+            break;
+        case 'P':
+            cout << "P is selected" << endl;
+            foundIndices = {foundIndices.at(0) - 1};
+            break;
+        case 'F':
+            cout << "F is selected" << endl;
+            foundIndices = {foundIndices.at(0) + 1};
+            break;
+        case 'A':
+            cout << "A is selected" << endl;
+            foundIndices = findText(bookArray, userSearchTerm);
+            break;
+        case 'X':
+            cout << "exiting the system" << endl;
+            isRunning = false;
+            break;
+    }
+
+    return foundIndices;
+}
+
+void printFromIndices(const string bookArray[ARRAYSIZE], vector<int> indices) {
+    for (int i =0; i < indices.size(); i++) {
+        int indexNum = indices.at(i);
+        cout << "Line " << indexNum << ':' << endl;
+        cout << bookArray[indexNum] << endl;
+    }
+}
 
 int main() {
     char userInput;
@@ -55,39 +103,25 @@ int main() {
 
     loadBook(bookArray);
 
+    //prompt(false);
+    //cin >> userInput;
+
+    foundIndices = getIndices(bookArray, userInput, isRunning);
+
+    printFromIndices(bookArray, foundIndices);
+
     while (isRunning) {
-        //prompt();
+        //prompt(true);
         //cin >> userInput;
-        userInput = 'W';
+        userInput = 'w';
 
-        switch(userInput) {
-            case 'W':
-                cout << "W is selected" << endl;
-                userSearchTerm = "five years";
-                foundIndices = findText(bookArray, userSearchTerm);
+        foundIndices = getIndices(bookArray, userInput, isRunning);
 
-                break;
-            case 'P':
-                cout << "P is selected" << endl;
-                break;
-            case 'F':
-                cout << "F is selected" << endl;
-                break;
-            case 'A':
-                cout << "A is selected" << endl;
-                break;
-            case 'X':
-                cout << "exiting the system" << endl;
-                break;
-        }
-        for (int i =0; i < foundIndices.size(); i++) {
-            cout << "in the repetion loop" << endl;
-            cout << bookArray[foundIndices.at(i)] << endl;
-        }
+        printFromIndices(bookArray, foundIndices);
         isRunning = false;
     }
 
-
+    return 0;
 }
 
 
